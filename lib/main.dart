1e7b2cb4bf151a880c656a211071c0d0a48e5539
S N Shekhar Das <sndasnsu@gmail.com>
2020-07-11 10:27:52 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Home Page"),
+          centerTitle: true,
+          leading: Icon(Icons.message),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.add_a_photo),
+              onPressed: () {},
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
